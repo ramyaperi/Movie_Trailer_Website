82513d1931d1bbdcee3e5ec6b03601eb6c70dd24
ramyaperi <30437760+ramyaperi@users.noreply.github.com>
2017-07-31 15:56:36 +0200

    alignment
    Changed font sizes and added code to show story line

--- fresh_tomatoes.py
+++ fresh_tomatoes.py
@@ -122,7 +122,8 @@ main_page_content = '''
 movie_tile_content = '''
 <div class="col-md-6 col-lg-4 movie-tile text-center" data-trailer-youtube-id="{trailer_youtube_id}" data-toggle="modal" data-target="#trailer">
     <img src="{poster_image_url}" width="220" height="342">
-    <h2>{movie_title}</h2>
+    <h3>{movie_title}</h3>
+    <font size="1">{movie_storyline}</font>
 </div>
 '''
 
@@ -142,6 +143,7 @@ def create_movie_tiles_content(movies):
         # Append the tile for the movie with its content filled in
         content += movie_tile_content.format(
             movie_title=movie.title,
+            movie_storyline = movie.storyline,
             poster_image_url=movie.poster_image_url,
             trailer_youtube_id=trailer_youtube_id
         )
